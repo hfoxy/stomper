41297e8bae758697e119efa1c4b1eb1561657f5b
Harry Fox <1254033+hfoxy@users.noreply.github.com>
2023-07-28 16:22:38 +0100

    remove example stuff

--- server/main.go
+++ server/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"flag"
-	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 	"stomper"
 )
+
+var version = "v0.0.1"
 
 var addr = flag.String("addr", "localhost:8448", "http service address")
 var compression = flag.String("compression", "true", "enable compression")
@@ -27,28 +28,8 @@ func main() {
 		Compression: comp == "true",
 	}
 
-	stompServer.AddConnectHandler(func(conn *websocket.Conn, request *http.Request, message *stomper.StompMessage) {
-		stompServer.Sugar.Infof("[connect] %s", conn.RemoteAddr())
-	})
-
-	stompServer.AddDisconnectHandler(func(conn *websocket.Conn) {
-		stompServer.Sugar.Infof("[disconnect] %s", conn.RemoteAddr())
-	})
-
-	stompServer.AddSubscribeHandler(func(conn *websocket.Conn, s string) {
-		stompServer.Sugar.Infof("[%s] [%s] subscribe", conn.RemoteAddr(), s)
-	})
-
-	stompServer.AddUnsubscribeHandler(func(conn *websocket.Conn, s string) {
-		stompServer.Sugar.Infof("[%s] [%s] unsubscribe", conn.RemoteAddr(), s)
-	})
-
-	stompServer.AddMessageHandler(func(conn *websocket.Conn, s string, message *stomper.StompMessage) {
-		stompServer.Sugar.Infof("[%s] [%s] recv: %s", conn.RemoteAddr(), s, string(*message.Body))
-	})
-
 	stompServer.Setup()
-	stompServer.Sugar.Infof("staring stomper...")
+	stompServer.Sugar.Infof("staring stomper %s...", version)
 
 	http.HandleFunc("/wss/websocket", stompServer.WssHandler)
 	http.HandleFunc("/health", healthHandler)
